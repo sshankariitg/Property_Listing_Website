7a9b0e66f6d21eaa754ed0ac44ea1f4a65e60140
Siddharth Shankar Pandey <81765776+sshankariitg@users.noreply.github.com>
2021-07-03 15:20:57 +0530

    Update settings.py

--- sidestate/settings.py
+++ sidestate/settings.py
@@ -131,9 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'sidestate/static')
-]
+#STATICFILES_DIRS=[
+   # os.path.join(BASE_DIR,'sidestate/static')
+#]
 #media folder
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
