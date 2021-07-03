940e47a32c761a0bf6b84afed9adae643b8707fe
Siddharth Shankar Pandey <81765776+sshankariitg@users.noreply.github.com>
2021-07-03 15:55:01 +0530

    Update settings.py

--- sidestate/settings.py
+++ sidestate/settings.py
@@ -129,15 +129,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS=[
+   os.path.join(BASE_DIR,'sidestate/static')
+]
+
 STATIC_URL = '/static/'
-#STATICFILES_DIRS=[
-   # os.path.join(BASE_DIR,'sidestate/static')
-#]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT=os.path.join(BASE_DIR,'live-static','static-root')
+
 #media folder
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
+
+MEDIA_ROOT=os.path.join(BASE_DIR,'live-static','media-root')
 
 #messages
 
